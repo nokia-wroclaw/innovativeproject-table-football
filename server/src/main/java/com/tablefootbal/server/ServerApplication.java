949d3a547cd98430c2c8aea2560c219861d63c2c
MichalMonko <michalmonko96@gmail.com>
2018-10-19 10:41:57 +0200

    Added keystore and https configuration

--- server/src/main/java/com/tablefootbal/server/ServerApplication.java
+++ server/src/main/java/com/tablefootbal/server/ServerApplication.java
@@ -2,11 +2,14 @@ package com.tablefootbal.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-public class ServerApplication {
-
-	public static void main(String[] args) {
+@EnableWebSecurity
+public class ServerApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ServerApplication.class, args);
 	}
 }
