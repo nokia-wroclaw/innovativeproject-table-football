bf58ad6aad53cce0633d3bc8fb4922781d939bc6
MichalMonko <michalmonko96@gmail.com>
2018-10-22 15:01:45 +0200

    Added Sensor Entity

---
+++ server/src/main/java/com/tablefootbal/server/entity/Sensor.java
@@ -0,0 +1,32 @@
+package com.tablefootbal.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "sensors")
+public class Sensor
+{
+	@Id()
+	String id;
+	
+	@Column(length = 1)
+	boolean isActive;
+	@Column(length = 1)
+	boolean isOnline;
+	
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	Date lastNotificationDate;
+	
+	@Column
+	int floor;
+	
+	@Column
+	int room;
+}
