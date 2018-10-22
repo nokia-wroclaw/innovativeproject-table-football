89340d9499c33322db2cb2f19ea8438831fe84de
MichalMonko <michalmonko96@gmail.com>
2018-10-22 16:10:36 +0200

    Configured in memory database and added sensors for testing purposes

--- server/src/main/java/com/tablefootbal/server/entity/Sensor.java
+++ server/src/main/java/com/tablefootbal/server/entity/Sensor.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Table(name = "sensors")
 public class Sensor
 {
-	@Id()
+	@Id
 	String id;
 	
 	@Column(length = 1)
---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('08:3F:33:33:3D:30', true,true,'2018-10-22 18:22:30', 1, 123)
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('03:F3:E3:A3:F3:C3', false,true,'2018-10-20 17:20:20', 1, 125)
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('02:F2:D2:32:F2:C2', false,false,'2018-10-22 18:22:30', 2, 222)
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('28:2F:23:23:2D:20', true,true,'2018-10-30 20:11:44', 2, 222)
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('18:1F:13:13:1D:10', false,true,'2018-09-22 11:22:31', 3, 331)
+INSERT INTO SENSORS (id,is_active,is_online,last_notification_date,floor,room) values ('01:F1:D1:31:F1:C1', false,false,'2018-10-10 10:15:10', 3, 385)
