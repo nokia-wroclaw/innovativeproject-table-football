bdcaebe7020ef0d168424b59262ab62ba47a4ba0
moorzyn <piotrk8998@gmail.com>
2018-11-12 21:18:44 +0100

    Resolve annotation conflict

--- server/src/test/java/com/tablefootbal/server/SensorRepositoryIntegrationTests.java
+++ server/src/test/java/com/tablefootbal/server/SensorRepositoryIntegrationTests.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@Transactional
 @SpringBootTest
 public class SensorRepositoryIntegrationTests
 {
