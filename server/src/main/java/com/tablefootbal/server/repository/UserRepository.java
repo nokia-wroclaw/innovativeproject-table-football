6ff37a2ac6ccb74754c2bab9cd71a24a5dcaf5a1
MichalMonko <michalmonko96@gmail.com>
2018-11-18 15:57:13 +0100

    Removed server files to make branch independent

--- server/src/main/java/com/tablefootbal/server/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tablefootbal.server.repository;
-
-import com.tablefootbal.server.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>
-{
-	User findByUsername(String username);
-}
