72fc8afed81c905cf841a9ec132f7161ae8c79ff
Kamil Turek <tazzu@live.com>
2018-12-21 23:08:50 +0100

    Config panel styling

--- frontend/src/app/config-panel/config-panel.component.ts
+++ frontend/src/app/config-panel/config-panel.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-config-panel',
   templateUrl: './config-panel.component.html',
-  styleUrls: ['./config-panel.component.css']
+  styleUrls: ['./config-panel.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ConfigPanelComponent implements OnInit {
   floors: number[];
