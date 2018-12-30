404998f15d52959c6a66b8e0ee427b47123ef91d
piotrkaron <zakoopovvy@gmail.com>
2018-12-30 17:14:17 +0100

    Init commit

---
+++ app/src/main/java/nokia/tablefootball/tablefootballandroid/MainActivity.kt
@@ -0,0 +1,38 @@
+package nokia.tablefootball.tablefootballandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.android.synthetic.main.main_activity.*
+
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main_activity)
+
+        main_webview.webViewClient = object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        main_webview.settings.javaScriptEnabled = true // ## Check JS vulnerabilities ##
+        main_webview.settings.setAppCacheEnabled(true)
+        main_webview.settings.domStorageEnabled = true
+        main_webview.loadUrl("http://192.168.43.188:4200")
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(this) {
+            var newToken : String = it.token
+            Log.e("TOKEN", newToken)
+        }
+    }
+}
---
+++ app/src/main/java/nokia/tablefootball/tablefootballandroid/MessagingHandlingService.kt
@@ -0,0 +1,74 @@
+package nokia.tablefootball.tablefootballandroid
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+open class MessagingHandlingService : FirebaseMessagingService() {
+
+    val TAG : String = "FIREBASE"
+
+    override fun onNewToken(token : String?){
+//        super.onNewToken(token)
+//        FirebaseInstanceId.getInstance().getToken()
+//        Log.e("NEW TOKEN", token)
+
+        Log.d(TAG,"Refreshed token: $token")
+    }
+
+//     override fun onMessageReceived(remoteMessage: RemoteMessage?){
+//        super.onMessageReceived(remoteMessage)
+//        Log.d(TAG, "From: ${remoteMessage?.from}")
+//        Log.d(TAG, "Notification msg body: ${remoteMessage?.notification?.body}")
+//    }
+
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val NOTIFICATION_CHANNEL_ID = "Nilesh_channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID, "Your Notifications", NotificationManager.IMPORTANCE_HIGH)
+
+            notificationChannel.description = "Description"
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.vibrationPattern = longArrayOf(0, 1000, 500, 1000)
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        // to diaplay notification in DND Mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
+            channel.canBypassDnd()
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+
+        notificationBuilder.setAutoCancel(true)
+            .setColor(ContextCompat.getColor(this, R.color.colorAccent))
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(remoteMessage!!.getNotification()!!.getBody())
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setAutoCancel(true)
+
+
+
+        notificationManager.notify(1000, notificationBuilder.build())
+
+    }
+}
