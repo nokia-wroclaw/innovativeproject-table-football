218b3e606e9d10124b623d8926bdf006ec1af8c8
piotrkaron <zakoopovvy@gmail.com>
2018-12-31 11:20:02 +0100

    Update listener for tests

--- backend/src/main/java/com/tablefootbal/server/notifications/GameStoppedListener.java
+++ backend/src/main/java/com/tablefootbal/server/notifications/GameStoppedListener.java
@@ -1,14 +1,32 @@
 package com.tablefootbal.server.notifications;
 
 import com.tablefootbal.server.events.PlayStoppedEvent;
+import com.tablefootbal.server.notifications.dto.Notification;
+import com.tablefootbal.server.notifications.dto.Push;
+import com.tablefootbal.server.notifications.service.NotificationSender;
+import com.tablefootbal.server.notifications.service.NotificationService;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GameStoppedListener implements ApplicationListener<PlayStoppedEvent> {
 
     @Override
     public void onApplicationEvent(PlayStoppedEvent event) {
 
         String sensorId = event.getSource().toString();
+
+        NotificationService service = new NotificationSender();
+
+        Notification notification = new Notification();
+        notification.setTitle("Zmiana statusu");
+        notification.setBody("Stop meczu na stole:" + sensorId);
+        Push push = new Push();
+        push.setTo(KeyStorage.deviceToken);
+        push.setNotification(notification);
+        push.setPriority("HIGH");
+
+        service.sendNotification(push);
 
     }
 }
