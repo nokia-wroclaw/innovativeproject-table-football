55f0ebac48d2ad2bc1538464b38383d882ca9b21
MichalMonko <michalmonko96@gmail.com>
2019-01-02 17:26:59 +0100

    Added additional parameters to admin panel page

--- frontend/src/app/model/algorithm-parameters.ts
+++ frontend/src/app/model/algorithm-parameters.ts
@@ -1,15 +1,19 @@
 export class AlgorithmParameters {
-    window_size: number;
-    threshold: number;
-    axis: string;
-    peaks_count: number;
-    max_readings: number;
+  window_size: number;
+  threshold: number;
+  axis: string;
+  peaks_count: number;
+  max_readings: number;
+  seconds_till_offline: number;
+  states_to_swap: number;
 
-    constructor() {
-        this.window_size = 0;
-        this.threshold = 0;
-        this.peaks_count = 0;
-        this.max_readings = 0;
-        this.axis = 'X';
-    }
+  constructor() {
+    this.window_size = 0;
+    this.threshold = 0;
+    this.peaks_count = 0;
+    this.max_readings = 0;
+    this.seconds_till_offline = 0;
+    this.states_to_swap = 0;
+    this.axis = 'X';
+  }
 }
